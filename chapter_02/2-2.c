8ec34b6881d1c1ee8139d2459b02c2e36119cfed
XSJJ <56115666@qq.com>
2018-10-08 11:14:45 +0800

    fix

--- chapter_02/2-2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("My Name : desuai zhen\n");
-	printf("My address : I won't tell you.");
-	
-	getchar();//避免程序一运行就结束退出
-	return 0;
-}
--- chapter_02/2-6.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int toes = 10;
-	printf("toes = %d\ntoes * 2 = %d\ntoes ^ 2 = %d", toes, toes * 2, toes*toes);
-	getchar();//避免程序一运行就结束退出
-	return 0;
-}
